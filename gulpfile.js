11bdc216650d0b50ae9c52b81004c2e25c9657f0
tdascoli <R0hrch@ch>
2022-09-18 10:58:58 +0200

    add some Gutenberg staff

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    minify      = require('gulp-minifier'),
+    zip         = require('gulp-zip'),
+    browserSync = require('browser-sync'),
+    del         = require('del');
+
+// BrowserSync Reload
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+gulp.task('watch', function (done) {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false});
+
+    gulp.watch("./src/**/*.scss", gulp.series('sass'));
+    gulp.watch("./app/*.html", reload);
+    done();
+});
+
+gulp.task('zip', function() {
+    return gulp.src(['dist/**/*.css'])
+        .pipe(zip('simple-unigrid.zip'))
+        .pipe(gulp.dest('app/'));
+});
+
+// Compile sass into CSS
+gulp.task('sass', function() {
+    return gulp.src('src/simple-unigrid.scss')
+        .pipe(sass())
+        .pipe(minify({
+          minify: true,
+          minifyCSS: true,
+          getKeptComment: function (content, filePath) {
+              var m = content.match(/\/\*![\s\S]*?\*\//img);
+              return m && m.join('\n') + '\n' || '';
+          }
+        }))
+        .pipe(gulp.dest("dist/"))
+        .pipe(gulp.dest("app/"))
+        .pipe(browserSync.reload({stream:true}));
+
+});
+
+gulp.task('clean', function () {
+    return del(['dist/**/*']);
+});
+
+gulp.task('default', gulp.series('sass','watch'));
+
+gulp.task('dist', gulp.series('clean','sass','zip'));
